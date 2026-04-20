a31e3c40c265b3ef40fe61032712b9b1f8ab865b
Wessti <80595220+a-nikityuk@users.noreply.github.com>
2026-04-20 10:57:41 +0300

    Lab-10 (20.04.2026)

---
+++ BIVT-25-Lab-10/Lab9/Purple/Task1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Purple
+{
+    public class Task1 : Purple
+    {
+        private string _outPut;
+
+        public string Output => _outPut;
+        public Task1(string input) : base(input) 
+        {
+            _outPut = "";
+        }
+        
+
+        public override void Review()
+        {
+
+            var res = new StringBuilder();
+            var array = Input.Split(' ');
+            foreach (var word in array)
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    if ((char.IsLetter(word[i]) && !char.IsDigit(word[i]) && word[i] != '\n' && word[i] != '\r') || (word[i] == '\'') || (word[i] == '-'))
+                        if (i > 2 && word[i] == 'h' && word[i - 1] == 't' && char.IsDigit(word[i - 2]))
+                        {
+                            res.Append("th");
+                            i--;
+                        }
+                        else
+                            res.Append(word[i]);
+                }
+                res.Append(" ");
+            }
+
+            for (int i = 0; i < Input.Length; i++)
+            {
+                if ((char.IsPunctuation(Input[i]) || char.IsDigit(Input[i]) || Input[i] == '\n' || Input[i] == '\r') && (Input[i] != '\'') && (Input[i] != '-'))
+                {
+                    res.Insert(i, Input[i]);
+                }
+
+            }
+            _outPut += res.ToString().Trim();
+
+        }
+
+        public override string ToString() {
+            return _outPut;
+        }
+    }
+}
---
+++ BIVT-25-Lab-10/Lab9/Purple/Task2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Purple
+{
+    public class Task2: Purple
+    {
+        private string[] _outPut;
+
+        public string[] Output => _outPut.ToArray();
+
+        public Task2(string input) : base(input)
+        {
+            _outPut = new string[0];
+        }
+
+        public override void Review()
+        {
+            var makeRows = new StringBuilder();
+            var array = Input.Split();
+
+            int lenOfString = 0;
+            foreach (var word in array)
+            {
+                lenOfString += word.Length + 1;
+                if (lenOfString <= 50)
+                {
+                    makeRows.Append(word + " ");
+                }
+                else
+                {
+                    makeRows.Append(Environment.NewLine);
+                    makeRows.Append(word + " ");
+                    lenOfString = word.Length;
+                }
+            }
+
+            string sWithNewLines = makeRows.ToString().Trim();
+
+            var splitByRows = sWithNewLines.Split(Environment.NewLine);
+
+            var numOfTabsInEveryRow = new int[0];
+            for (int i = 0; i < splitByRows.Length; i++)
+            {
+                splitByRows[i] = splitByRows[i].Trim();
+            }
+
+            for (int row = 0; row < splitByRows.Length; row++)
+            {
+                while (splitByRows[row].Length != 50)
+                {
+                    for (int i = 0; i < splitByRows[row].Length - 1; i++)
+                    {
+                        if (splitByRows[row].Length == 50)
+                            break;
+                        if (splitByRows[row][i] == ' ' && splitByRows[row][i + 1] != ' ')
+                        {
+                            splitByRows[row] = splitByRows[row].Substring(0, i) + "  " + splitByRows[row].Substring(i + 1);
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            Array.Resize(ref _outPut, splitByRows.Length);
+            for (int  i = 0;  i < splitByRows.Length;  i++)
+                _outPut[i] = splitByRows[i];
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (var item in Output)
+            {
+                s = s + item + Environment.NewLine;
+            }
+
+            return s;
+        }
+    }
+}
