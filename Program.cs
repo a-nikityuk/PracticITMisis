1bc7c4216fac35346e292c33dfc5f337d2762840
Wessti <80595220+a-nikityuk@users.noreply.github.com>
2026-05-17 20:25:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Classwork_28._04_Serialization;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ser = new MySerializator();
+            ser.Serialize();
+            ser.Deserialize();
+
+            var serX = new MyXMLSerializator();
+            serX.Serialize();
+            serX.Deserialize();
+                 
+        }
+    }
+    public class Student
+    {
+        private static int _counter = 0;
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public Subject[] Subjects { get; private set; }
+        public Student(string name, string surname)
+        {
+            Id = _counter++;
+            Name = name;
+            Surname = surname;
+            Subjects = new Subject[3]
+            {
+                new Subject("Math"),
+                new Subject("CS"),
+                new Subject("History")
+            };
+        }
+        [JsonConstructor]
+        [Newtonsoft.Json.JsonConstructor]
+        public Student(int id, string name, string surname, Subject[] subjects)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Subjects = subjects;
+        }
+        public void AddMarks(string subjectName, int[] marks)
+        {
+            for (int i = 0; i < Subjects.Length; i++)
+                if (Subjects[i].Name == subjectName)
+                    Subjects[i].AddMark(marks);
+        }
+        public void AddMarks(Subject subject, int[] marks)
+        {
+            for (int i = 0; i < Subjects.Length; i++)
+                if (Subjects[i].Name == subject.Name)
+                    Subjects[i].AddMark(marks);
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"{Id} {Name} has marks: " +
+            $"{string.Join(" ", Subjects.Select(x => x.FinalMark))}");
+            foreach (var s in Subjects)
+            {
+                Console.WriteLine(string.Join(" ", s.FinalMark));
+                if (s is Course c)
+                {
+                    Console.WriteLine(c.Duration);
+                }
+            }
+        }
+    }
+    public class Subject
+    {
+        private List<int> _marks;
+        public string Name { get; private set; }
+        public int[] Marks => _marks.ToArray();
+        public int FinalMark
+        {
+            get
+            {
+                if (_marks != null && _marks.Count > 0)
+                {
+                    return (int)Math.Round(_marks.Average());
+                }
+                return 0;
+            }
+        }
+        public Subject(string name)
+        {
+            Name = name;
+            _marks = new List<int>();
+        }
+        [JsonConstructor]
+        [Newtonsoft.Json.JsonConstructor]
+        public Subject(string name, int[] marks)
+        {
+            Name = name;
+            _marks = new List<int>();
+            _marks.AddRange(marks);
+        }
+        public void AddMark(int mark)
+        {
+            _marks.Add(mark);
+        }
+        public void AddMark(int[] mark)
+        {
+            _marks.AddRange(mark);
+        }
+
+        
+    }
+    public class Course : Subject
+    {
+        public int Duration { get; private set; }
+
+        public Course(string name, int duration) : base(name)
+        {
+            Duration = duration;
+        }
+
+        public Course(string name, int[] marks, int duration) : base(name, marks)
+        {
+            Duration = duration;
+        }
+    }
+}
---
+++ Srialazer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    internal class Srialazer
+    {
+        string _desktopPath;
+        string _path;
+        List<Student> _students;
+        public Srialazer()
+        {
+            _desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            _path = Path.Combine(_desktopPath, "example.json");
+
+            _students = new List<Student>(3)
+            {
+                new Student("A", "AA"),
+                new Student("B", "BB"),
+                new Student("C", "CC")
+            };
+
+            _students[0].AddMarks("Math", new int[] { 1, 2, 5, 3 });
+            _students[1].AddMarks("Math", new int[] { 2, 3, 4 });
+            _students[0].AddMarks("CS", new int[] { 1, 2, 5, 3 });
+            _students[0].AddMarks("CS", new int[] { 5, 3, 2, 4 });
+        }
+        public void Serialize()
+        {
+            var jsonString = JsonSerializer.Serialize(_students);
+
+            Console.WriteLine(jsonString);
+            File.WriteAllText(_path, jsonString);
+
+            _students = null;
+        }
+
+        public void Deserialize() {
+            Console.WriteLine();
+            var jsonString = File.ReadAllText(_path);
+            Console.WriteLine(jsonString);
+
+            Student[] students = JsonSerializer.Deserialize<Student[]>(jsonString);
+            foreach (var student in students) {
+                Console.WriteLine($"{student.Id} {student.Name} has marks: {string.Join(" ", student.Subjects.Select(x => x.FinalMark))}");
+            }
+        }
+        
+    }
+}
